1ecc80a80e47a796e1b3b16526037e252b90ba72
Akihiro Nakao <genn777f3@gmail.com>
2016-12-04 13:37:41 +0900

    set capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,33 @@ lock '3.6.0'
 set :application, 'my_app_name'
 set :repo_url, 'git@example.com:me/my_repo.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+lock '3.6.0'
+
+# デプロイするアプリケーション名
+set :application, 'portfolio'
+
+# cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
+set :repo_url, 'https://github.com/xxxxxxxx/yyyyyyyy'
+
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, ENV['BRANCH'] || 'master'
+
+# deploy先のディレクトリ。
+set :deploy_to, '/var/www/portfolio'
+
+# シンボリックリンクをはるフォルダ・ファイル
+set :linked_files, %w{.env config/secrets.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
+
+# 保持するバージョンの個数(※後述)
+set :keep_releases, 5
+
+# Rubyのバージョン
+set :rbenv_ruby, '2.3.0'
+set :rbenv_type, :system
+
+#出力するログのレベル。
+set :log_level, :debug
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
@@ -31,6 +56,3 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,16 +1,9 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '150.95.145.179', user: 'cent', roles: %w{app db web}
+# server '150.95.145.179', user: 'cent', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
-
-# role-based syntax
-# ==================
 
 # Defines a role with one or multiple servers. The primary server in each
 # group is considered to be the first unless any  hosts have the primary
@@ -46,6 +39,9 @@
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
+
+set :ssh_options, keys: '~/.ssh/aws'
+
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
