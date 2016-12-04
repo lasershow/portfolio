9029e1d21f173f280230150ebb5a3ff61515b4c8
Akihiro Nakao <genn777f3@gmail.com>
2016-12-04 14:51:11 +0900

    fix repo url2

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'portfolio'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/xxxxxxxx/yyyyyyyy'
+set :repo_url, 'git@github.com:lasershow/portfolio.git'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
